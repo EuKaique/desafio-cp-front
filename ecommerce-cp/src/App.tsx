0680d674ab56955eb4a377474a8f17bf407bd9af
Kaique <34290569+EuKaique@users.noreply.github.com>
2021-04-16 17:56:33 -0300

    commit 01

---
+++ ecommerce-cp/src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <header>
+      <Navbar/>
+      <Footer/>
+    </header>
+  );
+}
+
+export default App;
---
+++ ecommerce-cp/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>Ecommerce cp &copy; 2021</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
