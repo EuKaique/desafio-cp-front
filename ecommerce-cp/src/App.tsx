eabf794ca786d7f7e074da1d9cacdf1e02b8480c
Kaique <34290569+EuKaique@users.noreply.github.com>
2021-04-18 01:05:50 -0300

    commit 02

--- ecommerce-cp/src/App.tsx
+++ ecommerce-cp/src/App.tsx
@@ -1,12 +1,16 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Books from "./components/Books";
 
 function App() {
   return (
-    <header>
+    <div>
       <Navbar/>
+      <Banner/>
+      <Books/>
       <Footer/>
-    </header>
+    </div>
   );
 }
 
